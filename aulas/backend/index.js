632a7bf2159ba9037ee4d97ffeca478b03c1fef6
leitegs <leitedasilvag@gmail.com>
2020-03-23 14:29:00 -0300

    started project

---
+++ aulas/backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+    return res.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Gabriel Leite'
+    });
+});
+
+app.listen(3333);
